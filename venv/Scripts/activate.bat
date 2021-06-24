87f9e803f50968acac34f458e73a50d94b3ce7b2
manishnangliya <manunangliya@gmail.com>
2021-06-25 03:53:02 +0530

    final

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\manish\OneDrive\Desktop\try\venv
+set VIRTUAL_ENV=C:\Users\manish\OneDrive\Desktop\git\bookselling-\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
